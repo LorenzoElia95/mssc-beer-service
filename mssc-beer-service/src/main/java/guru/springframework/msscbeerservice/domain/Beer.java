52a5b8afc250dff9c870dcc54d1758966afeb469
lorenzo.elia <lorenzo.elia@rextart.com>
2023-10-25 10:29:21 +0200

    Beer Loader e bean validation

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -21,8 +21,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
-    private UUID io;
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
 
     @Version
     private Long version;
@@ -39,6 +39,7 @@ public class Beer {
 
     @Column(unique = true)
     private Long upc;
+
     private BigDecimal price;
     private Integer minOnHand;
     private Integer quantityToBrew;
